eb2d1d73222756c1befc5bbe08ce6530b7c1df38
WilliamGiorno <70899240+WilliamGiorno@users.noreply.github.com>
2023-05-05 18:53:34 +0200

    implement caching for tasks and elapsed times

--- public/script.js
+++ public/script.js
@@ -1,55 +1,58 @@
 document.getElementById("add-task").addEventListener("click", function() {
-    const taskName = document.getElementById("task-name").value;
-    if (taskName === '') return;
-    const taskList = document.getElementById("task-list");
-
-    const listItem = document.createElement("li");
-		listItem.classList.add("flex-container");
-
-    const taskLabel = document.createElement("span");
-    taskLabel.textContent = taskName;
-    listItem.appendChild(taskLabel);
-
-  	const startBtn = document.createElement("button");
-    startBtn.textContent = "Start";
-    startBtn.classList.add("start");
-
-		const stopBtn = document.createElement("button");
-    stopBtn.textContent = "Stop";
-    stopBtn.classList.add("stop");
-
-		const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Delete";
-    deleteBtn.classList.add("delete");
-    deleteBtn.addEventListener("click", function() {
-        taskList.removeChild(listItem);
-    });
-    listItem.appendChild(deleteBtn);
-
-    let timer;
-    let elapsedTime = 0;
-
-    startBtn.addEventListener("click", function() {
-        const startTime = Date.now() - elapsedTime;
-        timer = setInterval(function() {
-            elapsedTime = Date.now() - startTime;
-            const seconds = Math.floor(elapsedTime / 1000) % 60;
-            const minutes = Math.floor(elapsedTime / (1000 * 60)) % 60;
-            const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
-            taskLabel.textContent = `${taskName} - ${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-        }, 1000);
-    });
-
-    stopBtn.addEventListener("click", function() {
-        clearInterval(timer);
-    });
-    
-    listItem.appendChild(taskLabel);
-    listItem.appendChild(startBtn);
-    listItem.appendChild(stopBtn);
-    listItem.appendChild(deleteBtn);
-
-    taskList.appendChild(listItem);
-
-    document.getElementById("task-name").value = '';
+  const taskName = document.getElementById("task-name").value;
+  if (taskName === '') return;
+  addTaskToList(taskName, 0);
+  localStorage.setItem(taskName, 0);
 });
+
+function addTaskToList(taskName, initialElapsedTime) {
+  const taskList = document.getElementById("task-list");
+  const listItem = document.createElement("li");
+  listItem.classList.add("task-item");
+
+  const taskLabel = document.createElement("span");
+  taskLabel.textContent = taskName;
+  listItem.appendChild(taskLabel);
+
+  let elapsedTime = initialElapsedTime;
+
+  const startBtn = document.createElement("button");
+  startBtn.textContent = "Avvia";
+  let timer;
+  startBtn.addEventListener("click", function() {
+      timer = setInterval(function() {
+          elapsedTime += 1000;
+          taskLabel.textContent = `${taskName} - ${elapsedTime / 1000} s`;
+      }, 1000);
+  });
+  listItem.appendChild(startBtn);
+
+  const stopBtn = document.createElement("button");
+  stopBtn.textContent = "Ferma";
+  stopBtn.addEventListener("click", function() {
+      clearInterval(timer);
+      localStorage.setItem(taskName, elapsedTime);
+  });
+  listItem.appendChild(stopBtn);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Elimina";
+  deleteBtn.classList.add("delete");
+  deleteBtn.addEventListener("click", function() {
+      localStorage.removeItem(taskName);
+      taskList.removeChild(listItem);
+  });
+  listItem.appendChild(deleteBtn);
+
+  taskList.appendChild(listItem);
+}
+
+function loadTasksFromLocalStorage() {
+  for (let i = 0; i < localStorage.length; i++) {
+      const taskName = localStorage.key(i);
+      const elapsedTime = parseInt(localStorage.getItem(taskName), 10);
+      addTaskToList(taskName, elapsedTime);
+  }
+}
+
+loadTasksFromLocalStorage();
